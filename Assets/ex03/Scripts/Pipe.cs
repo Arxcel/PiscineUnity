3609d641fd59e30e2a7de5ff786929c1c992691e
vkozlov <vadim.k.394010@gmail.com>
2019-01-14 21:02:16 +0200

    [d00] ex02, ex03 finished

---
+++ Assets/ex03/Scripts/Pipe.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    public int Speed = 2;
+
+    private bool isGameRunning = true;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!isGameRunning)
+            return; 
+        transform.Translate(new Vector3(Speed * Time.deltaTime * -1f, 0));
+        if (transform.position.x < -4.5)
+            transform.position = new Vector3(4.5f, 0);
+    }
+
+    public void Stop()
+    {
+        isGameRunning = false;
+    }
+    
+}
